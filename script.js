0de835caea5d967e9222e48bdbd8f17ba0601844
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-10-08 11:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // write js code if required
-const flip = document.querySelector("#flip")
+const flip = document.querySelector(".flip")
 
 const initialimage = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
 const hoverimage = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
