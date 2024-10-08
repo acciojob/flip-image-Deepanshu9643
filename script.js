f9111c3d675da8a26822dd16af9c0d0182d3889a
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-10-08 11:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // write js code if required
+const flip = document.querySelector("#flip")
+
+const initialimage = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
+const hoverimage = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
+
+flip.addEventListener("mouseenter",() => {
+	flip.src =  hoverimage
+})
+flip.addEventListener("mouseleave",() => {
+	flip.src =  initialimage
+})
